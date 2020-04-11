d524366e3f8a40cce1edb753789f18e9c0d8f6b9
MichaelWoj <43684434+MichaelWoj@users.noreply.github.com>
2020-04-11 18:10:43 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from mycroft import MycroftSkill, intent_file_handler
+
+
+class Demo(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_file_handler('demo.intent')
+    def handle_test(self, message):
+        self.speak_dialog('demo')
+
+
+def create_skill():
+    return Demo()
